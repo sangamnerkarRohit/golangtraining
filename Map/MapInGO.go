f696ea6fecc42d088a218a7544f9aa8343b79b89
rohit <rohit.sangamnerkar@joshsoftware.com>
2022-05-26 22:28:28 +0530

    Map code changed added new code

--- Map/MapInGO.go
+++ Map/MapInGO.go
@@ -3,6 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	//allocating memory and initializing map using make function
 	m := make(map[string]int)
+	//if you want to add values then you need
+	m["Rohit"] = 45000
+	m["Vaibhav"] = 55000
+	m["Rajat"] = 45000
+	m["Sumit"] = 45000
 	fmt.Println(m)
+	//second way to init map is
+	m1 := map[string]int{
+		"Rohit":   1,
+		"Sumit":   2,
+		"Vaibhav": 3,
+		"Rajat":   4,
+	}
+
+	//print new map
+	fmt.Println(m1)
 }
